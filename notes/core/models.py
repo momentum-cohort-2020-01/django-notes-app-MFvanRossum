0087e5c504e70be604351b676ce9c62b699abe3d
MFvanRossum <matissev@hotmail.com>
2020-02-25 13:23:27 -0500

    Adds automatic DateTime Fields to show when notes are created and updated

--- notes/core/models.py
+++ notes/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=280)
+    body = models.TextField(max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Note Title: {self.title} Body: {self.body}"
--- notes/core/views.py
+++ notes/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import data 
+from .models import Note
 
 def notes_list(request):
-    notes = data.NOTES
+    notes = Note.objects.all()
     return render(request, 'core/notes_list.html', {'notes': notes})
+
+def notes_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'core/notes_detail.html', {'note': note})
